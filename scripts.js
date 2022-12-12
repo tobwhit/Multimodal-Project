3ffaba5a96a1cd18b935cb55335ad3a5c5144273
tobwhit <119770173+tobwhit@users.noreply.github.com>
2022-12-12 13:35:17 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// const typewriter = document.getElementById("typewriter");
+// const text = typewriter.innerText;
+// typewriter.innerText = "";
+// let i = 0;
+// const type = () => {
+//   if (i < text.length) {
+//     console.log(text.charAt(i));
+//     typewriter.innerText += text.charAt(i);
+
+//     i++;
+//     setTimeout(type, 25);
+//   }
+// };
+
+// type();
